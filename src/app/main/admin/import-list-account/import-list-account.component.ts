591653ebd912763166862c2c2ed9c64edf284090
Nguyen Dinh Liem <fakesdinhliem272@gmail.com>
2019-04-03 19:40:51 +0700

    Push Admin Branch

---
+++ src/app/main/admin/import-list-account/import-list-account.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient, HttpRequest, HttpResponse } from "@angular/common/http";
+import { NzMessageService, UploadFile } from "ng-zorro-antd";
+import { AdminServiceService } from "../admin-service.service";
+import { NotificationService } from "src/app/services/notification.service";
+
+@Component({
+  selector: "app-import-list-account",
+  templateUrl: "./import-list-account.component.html",
+  styleUrls: ["./import-list-account.component.css"]
+})
+export class ImportListAccountComponent implements OnInit {
+  uploading = false;
+  fileList: UploadFile[] = [];
+  errors = [];
+  loadingTable = false;
+  accounts = [];
+  constructor(
+    private http: AdminServiceService,
+    private notification: NotificationService,
+    private adminService: AdminServiceService
+  ) {}
+
+  beforeUpload = (file: UploadFile): boolean => {
+    this.fileList = this.fileList.concat(file);
+    return false;
+  };
+  handleUpload(): void {
+    this.uploading = true;
+    this.http.postFile(this.fileList[0]).subscribe(
+      res => {
+        if (res["code"] === 1) {
+          this.uploading = false;
+          this.notification.show("success", "Success", "Import file success");
+        } else {
+          this.uploading = false;
+          this.errors = res["error"];
+        }
+      },
+      err => {
+        console.log("err", err);
+        this.uploading = false;
+      }
+    );
+  }
+
+  getAccounts() {
+    this.loadingTable = true;
+    this.adminService.getAllAccount().subscribe(
+      success => {
+        if (success["code"] === 1) {
+          this.accounts = success["data"];
+          this.loadingTable = false;
+        }
+      },
+      err => {
+        console.log("err");
+        this.loadingTable = false;
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.getAccounts()
+  }
+}
